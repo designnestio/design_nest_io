2f3a6fa6a66e72d8a535f012698b21b7478e0dcf
designnestio <designnestio@gmail.com>
2018-06-24 07:46:44 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 from design_nest import __version__
 
-setup(name='designnest',
+setup(name='designNest',
       version=__version__,
-      description='Python library to generate building analysis models.',
+      description='Python library which allows to read, modify, create and run EnergyPlus files and simulations.',
       url='',
-      download_url='{}'.format(__version__),
+      download_url=f'{__version__}',
       author='https://designnest.io',
       author_email='designnestio@gmail.com',
       license='Apache License 2.0',
       classifiers=['Development Status :: 3 - Alpha'],
       packages=['design_nest'],
-      keywords=['EnergyPlus', 'epJSON', 'Load Calculation'])
+      keywords=['EnergyPlus', 'epJSON'])
