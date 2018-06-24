1c32222c616ab149f644d1f14172955a7024e06f
designnestio <designnestio@gmail.com>
2018-06-24 08:19:53 -0400

    update setup.py with long description and package name update

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup
 from design_nest import __version__
 
-setup(name='designNest',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='design_nest',
       version=__version__,
       description='Python library which allows to read, modify, create and run EnergyPlus files and simulations.',
-      url='',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/designnestio/design_nest_io',
       download_url=f'{__version__}',
       author='https://designnest.io',
       author_email='designnestio@gmail.com',
       license='Apache License 2.0',
-      classifiers=['Development Status :: 3 - Alpha'],
+      classifiers=(
+            'Development Status :: 3 - Alpha',
+            "Operating System :: OS Independent",
+      ),
       packages=['design_nest'],
       keywords=['EnergyPlus', 'epJSON'])
