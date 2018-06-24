8902ee173891e91402a01c2fd01c37794c644707
designnestio <designnestio@gmail.com>
2018-06-24 07:48:03 -0400

    class file generator template

--- generator/templates/class.py
+++ generator/templates/class.py
@@ -1,25 +1,15 @@
-class {{ obj.class_name }}:
-    """ Corresponds to object `{{ obj.internal_name }}`
-    {%- if obj.memo  %}
-        {{ memo }}
-    {%- endif %}
-    """
-    _schema = {{ obj.schema }}
-
+class {{obj.class_name}}(BaseObject):
+    """ Corresponds to object `{{ obj.eplus_name }}` """
+    _schema = {{obj.schema}}
 
     {%- for field in obj.fields %}
 
     @property
-    def {{field.field_name}}(self):
-        """field `{{ field.internal_name }}`
-        |  {{field.comment}}
-        """
-        return self["{{ field.internal_name }}"]
+    def {{field}}(self):
+        return self["{{ field }}"]
 
-    @{{field.field_name}}.setter
-    def {{field.field_name}}(self, value={%- if field.attributes.default and not field.attributes.pytype == "str" %}{{ field.attributes.default}}{% elif field.attributes.default and (field.attributes.pytype == "str") %}"{{field.attributes.default}}"{% else %}None{% endif %}):
-        """  Corresponds to field `{{field.internal_name}}`
-        """
-        self["{{ field.internal_name }}"] = value
+    @{{field}}.setter
+    def {{field}}(self, value=None):
+        self["{{ field }}"] = value
     {%- endfor %}
         
